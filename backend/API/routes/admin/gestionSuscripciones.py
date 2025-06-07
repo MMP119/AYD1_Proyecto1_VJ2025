c1a3250a24eeb43c0a77066d6eff777f1f3e17d0
Samuel MP <57497888+SmillerMP@users.noreply.github.com>
2025-06-07 11:59:57 -0600

    feat: (Frontend + Backend): Suscripciones Admin
    Se implemento el panel de suscripciones para el usuario administrador, donde podrá ver y filtrar las suscripciones presentes, ya sea por fecha, estado o categoría de suscripción
    
    feat: admin suscriptions panel

--- backend/API/routes/admin/gestionSuscripciones.py
+++ backend/API/routes/admin/gestionSuscripciones.py
@@ -11,18 +11,32 @@ por estado,tipo de servicio o fecha de vencimiento.
 
 router = APIRouter()
 
-@router.get("/admin/suscripciones")  # Listar suscripciones, se deben filtar en el frontend
+@router.get("/admin/suscripciones")  # Listar suscripciones, se deben filtrar en el frontend
 async def listar_suscripciones(request: Request):
     try:
         pool = await get_db_pool(request.app)
-
         async with pool.acquire() as conn:
             async with conn.cursor(aiomysql.DictCursor) as cursor:
-                await cursor.execute("SELECT * FROM Subscription")
+                await cursor.execute("""
+                    SELECT 
+                        s.SubscriptionId,
+                        u.Name AS user,
+                        u.Email AS email,
+                        sv.Name AS service,
+                        sv.Category AS category,
+                        s.StartDate,
+                        s.EndDate,
+                        s.Status,
+                        s.AmountPaid,
+                        s.PaymentMethod
+                    FROM Subscription s
+                    JOIN User u ON s.UserId = u.UserId
+                    JOIN Plan p ON s.PlanId = p.PlanId
+                    JOIN Service sv ON p.ServiceId = sv.ServiceId
+                """)
                 suscripciones = await cursor.fetchall()
                 return {"suscripciones": suscripciones}
-                
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al listar suscripciones: {str(e)}")
-    
+
 
